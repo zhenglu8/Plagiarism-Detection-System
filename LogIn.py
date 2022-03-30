54f7c90ae4fb224117fa27d8e23f7012a39818cc
daniel <47399125+zhenglu8@users.noreply.github.com>
2022-03-30 13:20:44 -0700

    added logo image on LogIn page

--- LogIn.py
+++ LogIn.py
@@ -3,10 +3,11 @@ from tkinter import ttk
 import pyrelog as pl
 from selenium import webdriver
 import webbrowser
+from PIL import Image, ImageTk
 
 window = Tk()
 window.title("Plagiarism Detection System")
-canvas = Canvas(window, width=600, height=400)
+canvas = Canvas(window, width=700, height=600)
 canvas.grid(columnspan=2, rowspan=6)
 
 # Define a clear function to clear all entries
@@ -57,52 +58,59 @@ def aboutus():
     import AboutUs
 
 
+logo = Image.open('logo.png')
+test = ImageTk.PhotoImage(logo)
+
+label0 = Label(image=test)
+label0.image = test
+label0.grid(row=0, columnspan=2)
+
 label1 = Label(window, text="Username", font=("Raleway", 15))
-label1.grid(row=0, column=0)
+label1.grid(row=1, column=0)
 
 label2 = Label(window, text="Password", font=("Raleway", 15))
-label2.grid(row=1, column=0)
+label2.grid(row=2, column=0)
 
 username_text = StringVar()
 entry1 = Entry(window, textvariable=username_text)
-entry1.grid(row=0, column=1)
+entry1.grid(row=1, column=1)
 
 password_text = StringVar()
 entry2 = Entry(window, textvariable=password_text)
-entry2.grid(row=1, column=1)
+entry2.grid(row=2, column=1)
 
 # Login button
 button1 = Button(window, text="Login", height=2, width=12,
                  font="Raleway", bg="#20bebe", fg="white", command=login)
-button1.grid(row=2, column=0)
+button1.grid(row=3, column=0)
 
 # Signup button
 button2 = Button(window, text="Signup", height=2, width=12,
                  font="Raleway", bg="#20bebe", fg="white", command=signup)
-button2.grid(row=3, column=0)
+button2.grid(row=4, column=0)
 
 button3 = Button(window, text="Google Login", height=2, width=12,
                  font="Raleway", bg="#20bebe", fg="white", command=google)
-button3.grid(row=2, column=1)
+button3.grid(row=3, column=1)
 
 button4 = Button(window, text="Facebook Login", height=2, width=12,
                  font="Raleway", bg="#20bebe", fg="white", command=facebook)
-button4.grid(row=3, column=1)
+button4.grid(row=4, column=1)
 
 # Clear All button
 button5 = Button(window, text="Clear All", height=2, width=12,
                  font="Raleway", bg="#20bebe", fg="white", command=clear)
-button5.grid(row=4, column=0)
+button5.grid(row=5, column=0)
 
 # About Us button
 button7 = Button(window, text="About Us", height=2, width=12,
                  font="Raleway", bg="#20bebe", fg="white", command=aboutus)
-button7.grid(row=4, column=1)
+button7.grid(row=5, column=1)
 
 # Exit button
 button6 = Button(window, text="Exit", height=2, width=12,
                  font="Raleway", bg="#20bebe", fg="white", command=lambda: window.destroy())
-button6.grid(row=5, column=0)
+button6.grid(row=6, column=0)
 
 
 window.mainloop()
